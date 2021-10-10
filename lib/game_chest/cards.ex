c8f6a2331246812cbc849f22147bfcfe1cf33065
jmcook413 <jeremycook290@gmail.com>
2021-10-10 14:38:03 -0500

    Adds cards module with standard deck function

---
+++ lib/game_chest/cards.ex
@@ -0,0 +1,9 @@
+defmodule GameChest.Cards do
+
+  def standard do
+    Enum.map(["spade", "club", "diamond", "heart"],
+      fn suit -> List.zip([List.duplicate(suit, 14), Enum.to_list(1..14)]) end
+    )
+    |> List.flatten()
+  end
+end
