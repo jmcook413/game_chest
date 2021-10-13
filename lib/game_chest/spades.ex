0c01627cc5164c66ad9d1dba5f0946281d8a60ed
jmcook413 <jeremycook290@gmail.com>
2021-10-12 21:48:20 -0500

    Adds start and deal functions to setup game

--- lib/game_chest/spades.ex
+++ lib/game_chest/spades.ex
@@ -2,5 +2,22 @@ defmodule GameChest.Spades do
   alias GameChest.Cards
   alias GameChest.Dealer
 
-  defstruct [:hands, :teams, :scores, :bids]
+  defstruct [:game_id, :hands, :teams, :scores, :bids]
+
+  def start(players) do
+    %{deck: _deck, hands: hands} = deal(players)
+    teams = [team1, team2] = Enum.chunk_every(players, 2) |> Enum.zip()
+
+    %__MODULE__{
+      hands: hands,
+      teams: teams,
+      scores: [{team1, 0}, {team2, 0}]
+    }
+  end
+
+  defp deal(players) do
+    Cards.standard()
+    |> Dealer.shuffle()
+    |> Dealer.deal(players)
+  end
 end
